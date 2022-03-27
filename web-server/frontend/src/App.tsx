eaaa13da0d5a6af10989df30359f44b260f9ffa1
nikelborm <29357907+nikelborm@users.noreply.github.com>
2022-03-27 15:13:43 +0300

    asd

--- web-server/frontend/src/App.tsx
+++ web-server/frontend/src/App.tsx
@@ -10,34 +10,36 @@ import {
   RoutesGraph,
 } from 'pages';
 import Clients from 'pages/Clients/Clients';
+import { usePath } from 'hooks';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Root />}></Route>
+      <Route path="/" element={<Root />} />
+
       <Route path="/auth" element={<AuthWrapper />}>
-        <Route path="login" element={<Login />}></Route>
-        <Route path="registration" element={<Register />}></Route>
+        <Route path="login" element={<Login />} />
+        <Route path="registration" element={<Register />} />
         <Route path="*" element={<Navigate to="login" />} />
       </Route>
+
       <Route path="/adminPanel" element={<AdminPanelWrapper />}>
-        <Route path="profile" element={<Profile />}></Route>
-        <Route path="clients" element={<Clients />}></Route>
-        <Route path="routesGraph" element={<RoutesGraph />}></Route>
-        <Route path="events" element={<Events />}></Route>
-        <Route path="messages" element={<Messages />}></Route>
+        <Route path="profile" element={<Profile />} />
+        <Route path="clients" element={<Clients />} />
+        <Route path="routesGraph" element={<RoutesGraph />} />
+        <Route path="events" element={<Events />} />
+        <Route path="messages" element={<Messages />} />
         <Route path="*" element={<Navigate to="profile" />} />
       </Route>
+
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
 
 function AuthWrapper() {
-  const isAdminPanelOpened = useMatch('auth');
-  if (isAdminPanelOpened) {
-    return <Navigate to="login" />;
-  }
+  const { deepestPathPart } = usePath();
+  if (deepestPathPart === 'auth') return <Navigate to="login" />;
 
   return (
     <div>
