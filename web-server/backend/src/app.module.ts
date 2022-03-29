ed7cc9ef8fe8d006718b914fef95a16dd72d597c
nikelborm <29357907+nikelborm@users.noreply.github.com>
2022-03-29 13:50:17 +0300

    asd

--- web-server/backend/src/app.module.ts
+++ web-server/backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { appConfig, dbConfig } from './config';
 import { AccessLogMiddleware } from './tools';
 import { UserModule } from './modules/user';
 import { InfrastructureModule } from './modules/infrastructure';
+import { join, resolve } from 'path';
 
 @Module({
   imports: [
@@ -11,6 +13,9 @@ import { InfrastructureModule } from './modules/infrastructure';
       cache: true,
       isGlobal: true,
       load: [appConfig, dbConfig],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(resolve(), 'build'),
     }),
     InfrastructureModule,
     UserModule,
