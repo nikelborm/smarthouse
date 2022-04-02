529d8dd802a124c7c26eae28ec48e1ec458507bd
nikelborm <29357907+nikelborm@users.noreply.github.com>
2022-04-02 18:37:24 +0300

    before ws

--- gateway/backend/src/modules/encryption/encryption.useCase.ts
+++ gateway/backend/src/modules/encryption/encryption.useCase.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { repo } from '../infrastructure';
+import { IEncryptionWorker } from './IEncryptionWorker';
+import * as workers from './workers';
 
 @Injectable()
 export class EncryptionUseCase {
-  constructor(private readonly routeRepo: repo.RouteRepo) {}
+  constructor() {
+    for (const Worker of Object.values(workers)) {
+      const workerInstance = Object.freeze(new Worker());
+      this.store[workerInstance.uuid] = workerInstance;
+    }
+  }
+
+  getEncryptionWorker(uuid: string) {
+    return this.store[uuid];
+  }
+
+  private store: {
+    [uuid in string]: IEncryptionWorker<any, any, any>;
+  } = Object.create(null);
 }
--- gateway/backend/src/modules/infrastructure/model/encryptionWorker.model.ts
+++ gateway/backend/src/modules/infrastructure/model/encryptionWorker.model.ts
@@ -23,8 +23,3 @@ export class EncryptionWorker {
   @OneToMany(() => Client, (client) => client.encryptionWorker)
   clients!: Client[];
 }
-
-// По умолчанию будут включены следующие алгоритмы шифрования
-// 'nothing': 'ca4e23ec-f2a4-4d78-aa94-2065d72d5824'
-// 'rsa512': '92782e54-ac5b-4787-b940-2fd0978fc4b8'
-// 'aes128': '6b8d962d-ac90-4719-8ae8-a5da728b750d'
