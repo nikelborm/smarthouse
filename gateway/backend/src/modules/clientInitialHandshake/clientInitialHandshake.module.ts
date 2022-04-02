b07306290eed9c7d63954a2601949490124f4fd6
nikelborm <r0075301net.r@yandex.ru>
2022-04-02 15:24:30 +0300

    asd

--- gateway/backend/src/modules/clientInitialHandshake/clientInitialHandshake.module.ts
+++ gateway/backend/src/modules/clientInitialHandshake/clientInitialHandshake.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ClientInitialHandshakeUseCase } from './clientInitialHandshake.useCase';
+import { ClientInitialHardwareHandshakeService } from './clientInitialHardwareHandshake.service';
 import { USBSerialDeviceWatcherService } from './USBSerialDeviceWatcher.service';
 
 @Module({
-  providers: [ClientInitialHandshakeUseCase, USBSerialDeviceWatcherService],
+  providers: [
+    ClientInitialHandshakeUseCase,
+    ClientInitialHardwareHandshakeService,
+    USBSerialDeviceWatcherService,
+  ],
   exports: [ClientInitialHandshakeUseCase],
 })
 export class ClientInitialHandshakeModule {}
