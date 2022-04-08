318eb7c70ca91dffe128598d279d4772dfb298d2
Andrew Klevcov <andrew.klevcov@gmail.com>
2022-04-09 01:16:25 +0300

    ...

--- web-server/frontend/src/pages/Clients/Clients.tsx
+++ web-server/frontend/src/pages/Clients/Clients.tsx
@@ -1,5 +1,53 @@
 import React from 'react';
+import { Table, Space } from 'antd';
+import { Tag, Divider } from 'antd';
+const { Column, ColumnGroup } = Table;
+
+const data = [
+  {
+    id:'1',
+    uuid:'974ce0ad-5dbe-4685-9ae7-4ffd8285ce3e',
+    shortname:'Andrew',
+	fullname:'Andrew Klevtsov',
+    description:'default user',
+    obligatory:['open door'],
+    online:true,
+  },
+  {
+    id:'2',
+    uuid:'7fcc2537-424d-400c-9f10-aba0fe55c36c',
+    shortname:'Dmitiry',
+	fullname:'Dmitriy Gurevnin',
+    description:'default user',
+    online: false,
+  },
+  {
+    id:'3',
+    uuid:'bda0e5c3-1bc9-4c9a-919f-0058c9e5d8c2',
+    shortname:'Nikolay',
+	fullname:'Nikolay Bormotov',
+    description:'default user',
+    online: false,
+  },
+  {
+    id:'4',
+    uuid:'a2293aaf-1767-4e6e-ac56-04336f2d3302',
+    shortname:'Nikita',
+	fullname:'Nikita Davidchuk',
+    description:'default user',
+    online: false,
+  },
+];
 
 export const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <Table dataSource={data}>
+      <Column title="ID" dataIndex="id" key="id" />
+      <Column title="UUID" dataIndex="uuid" key="uuid" />
+      <Column title="Short name" dataIndex="shortname"  key="shortname"/>
+	  <Column title="Full name" dataIndex="fullname"  key="fullname"/>
+      <Column title="Description" dataIndex="description" key="description" />
+      <Column title="Online" dataIndex="online" key="online"/>
+    </Table>
+  );
 };
